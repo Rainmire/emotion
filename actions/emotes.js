881113048e3602b0e43da3075d2a5cc43dcc9b80
Rainmire <mqyin@g.ucla.edu>
2018-02-26 12:00:31 -0800

    add emote fetch

--- actions/emotes.js
+++ actions/emotes.js
@@ -10,9 +10,9 @@ emoteAction = (bot, channelID, message, evt) => {
 
   let images = evt.d.attachments
   if (images.length !== 0) {
-    addEmote(cmd, images[0].url, bot);
+    addEmote(cmd, images[0].url);
   } else {
-    sendEmote(cmd);
+    sendEmote(cmd, bot, channelID);
   }
 
 }
@@ -25,11 +25,37 @@ addEmote = (cmd, url) => {
   console.log(`New emote saved: ${cmd}`);
 }
 
-sendEmote = (cmd) => {
-  console.log(`Emote sent: ${cmd}`);
-}
+sendEmote = (cmd, bot, channelID) => {
+  let emote = Emote.find({command: cmd}, 'imageUrl', (err, result) => {
+    if (err) {
+      console.log(`error: ${err}`);
+    }
+    else if (result.length === 0) {
+      bot.sendMessage({
+        to: channelID,
+        message: `"!${cmd}" is not a valid command`
+      });
+    }
+    else {
+      bot.sendMessage({
+        to: channelID,
+        message: result[0].imageUrl
+      });
+    }
+  })
 
-// bot.sendMessage({
-//   to: channelID,
-//   message: img
-// });
+  // console.log(`cmd: ${cmd}`);
+  // console.log(`Find: ${Emote.find({command: cmd})}`);
+  // console.log(`First: ${Emote.find({command: cmd})[0]}`);
+
+  // console.log(emote);
+
+  // let url = emote.imageUrl;
+
+  // bot.sendMessage({
+  //   to: channelID,
+  //   message: url
+  // });
+
+  // console.log(`Emote sent: ${url}`);
+}
