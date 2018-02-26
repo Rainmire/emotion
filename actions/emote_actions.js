8e1d598dbf327f9fa066e3a8ad27906836cf5319
Rainmire <mqyin@g.ucla.edu>
2018-02-26 13:12:50 -0800

    simplify sendEmote

--- actions/emote_actions.js
+++ actions/emote_actions.js
@@ -8,40 +8,8 @@ emoteAction = (bot, channelID, message, evt) => {
   let args = message.substring(1).split(' ');
   let cmd = args[0];  
 
-  let images = evt.d.attachments
-  if (images.length !== 0) {
-    addEmote(cmd, images[0].url, bot, channelID);
-  } else {
-    sendEmote(cmd, bot, channelID);
-  }
-
-}
-
-addEmote = (cmd, url, bot, channelID) => {
-
-  let emote = Emote.find({command: cmd}, (err, result) => {
-    if (err) {
-      bot.sendMessage({
-        to: channelID,
-        message: err
-      });
-    } else if (result.length === 0) {
-      new Emote({
-        command: cmd,
-        imageUrl: url
-      }).save()
-      bot.sendMessage({
-        to: channelID,
-        message: `New emote "!${cmd}" saved!`
-      });
-    } else {
-      bot.sendMessage({
-        to: channelID,
-        message: `Emote "!${cmd}" already exists.`
-      });
-      
-    }
-  })
+  sendEmote(cmd, bot, channelID);
+  
 }
 
 sendEmote = (cmd, bot, channelID) => {
@@ -51,12 +19,7 @@ sendEmote = (cmd, bot, channelID) => {
         to: channelID,
         message: err
       });
-    } else if (result.length === 0) {
-      bot.sendMessage({
-        to: channelID,
-        message: `"!${cmd}" is not a valid command.`
-      });
-    } else {
+    } else if (result.length !== 0){
       bot.sendMessage({
         to: channelID,
         message: result[0].imageUrl
