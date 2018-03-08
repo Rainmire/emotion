411a1fd317a69aaa7216a8c0540f5cb5d438e886
Rainmire <mqyin@g.ucla.edu>
2018-03-08 13:43:24 -0800

    update gitignore to include scripts and backups

--- models/Emote.js
+++ models/Emote.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const emoteSchema = new Schema({
   command:{
     type: String,
-    required: true
+    required: true,
+    index: true
   },
   imageUrl:{
     type: String,
