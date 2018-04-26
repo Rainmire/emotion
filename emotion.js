0b0ac668597b437d05696fa8ee64c53cd16afedf
Mitch Yin <mitch.y@taboola.com>
2018-04-26 11:51:34 -0700

    upgrade to discord.js

--- emotion.js
+++ emotion.js
@@ -1,44 +1,61 @@
 #!/usr/bin/env node
-const Discord = require('discord.io');
-const logger = require('winston');
-const mongoose = require('mongoose');
+// const Discord = require('discord.io');
+const Discord = require('discord.js');
+
+// const logger = require('winston');
+// const mongoose = require('mongoose');
 
 // Load routers
-const actionRouter = require('./routers/action_router');
+// const actionRouter = require('./routers/action_router');
 
 // DB Config
-const db = require('./config/database');
+// const db = require('./config/database');
 // Map global promise - get rid of warning
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 // Connect to mongoose
-mongoose.connect(db.mongoURI, {
-  useMongoClient: true
-})
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+// mongoose.connect(db.mongoURI, {
+//   useMongoClient: true
+// })
+//   .then(() => console.log('MongoDB Connected...'))
+//   .catch(err => console.log(err));
 
 // Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-  colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-const dc = require('./config/discord');
-const bot = new Discord.Client({
-  token: dc.discordToken,
-  autorun: true
-});
-bot.on('ready', function (evt) {
-  logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(bot.username + ' - (' + bot.id + ')');
+// logger.remove(logger.transports.Console);
+// logger.add(logger.transports.Console, {
+//   colorize: true
+// });
+// logger.level = 'debug';
+// Initialize Discord client
+const discordConfig = require('./config/discord');
+// const client = new Discord.Client({
+//   token: dc.discordToken,
+//   autorun: true
+// });
+// client.on('ready', function (evt) {
+//   logger.info('Connected');
+//   logger.info('Logged in as: ');
+//   logger.info(client.username + ' - (' + client.id + ')');
+// });
+
+// client.on('message', function (user, userId, channelId, message, evt) {
+//   actionRouter(client, channelId, message, evt);
+// });
+
+// client.on('disconnect', function(errMsg, code) {
+//   console.log(errMsg);
+// });
+
+//using discord.js
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-bot.on('message', function (user, userId, channelId, message, evt) {
-  actionRouter(bot, channelId, message, evt);
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
-bot.on('disconnect', function(errMsg, code) {
-  console.log(errMsg);
-});
+client.login(discordConfig.discordToken);
